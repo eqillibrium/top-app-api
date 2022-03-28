a6ac3c1d526b12b0a6f3bfba4cca0143451bd7dd
Eqillibrium <eqillibrium@live.ru>
2022-03-28 12:51:10 +0300

    mongoDB connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { AuthModule } from './auth/auth.module';
 import { PageModule } from './page/page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './config/mongo.config'
 
 @Module({
-  imports: [AuthModule, PageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig
+    }),
+    AuthModule,
+    PageModule,
+    ProductModule,
+    ReviewModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
